d1a4aabbbec0c6d2d2c278219732f79cf7c57e47
Thanadol Chomphoochan <aquablitz11@gmail.com>
2017-03-01 12:42:08 +0700

    Define callback functions

--- CGGCTF/CTFPlugin.cs
+++ CGGCTF/CTFPlugin.cs
@@ -27,6 +27,9 @@ namespace CGGCTF
         TeamColor[] playerColor = new TeamColor[256];
         bool[] playerPvP = new bool[256];
         PlayerData[] originalChar = new PlayerData[256];
+        Dictionary<int, int> revID = new Dictionary<int, int>(); // user ID to index lookup
+
+        dynamic redSpawn, blueSpawn; // TODO - get properly data type
 
         #region Initialization
 
@@ -59,6 +62,132 @@ namespace CGGCTF
         {
             // callbacks
             CTFCallback cb = new CTFCallback();
+
+            cb.setTeam = delegate (int id, CTFTeam team) {
+                TeamColor color = TeamColor.White;
+                if (team == CTFTeam.Red)
+                    color = TeamColor.Red;
+                else if (team == CTFTeam.Blue)
+                    color = TeamColor.Blue;
+                setTeam(revID[id], color);
+            };
+            cb.setPvP = delegate (int id, bool pvp) {
+                setPvP(revID[id], pvp);
+            };
+            cb.setInventory = delegate (int id, PlayerData inventory) {
+                // TODO
+                throw new NotImplementedException();
+            };
+            cb.saveInventory = delegate (int id) {
+                // TODO
+                throw new NotImplementedException();
+            };
+            cb.warpToSpawn = delegate (int id, CTFTeam team) {
+                var tplr = TShock.Players[revID[id]];
+                if (team == CTFTeam.Red)
+                    tplr.Teleport(redSpawn.X, redSpawn.Y);
+                else if (team == CTFTeam.Blue)
+                    tplr.Teleport(blueSpawn.X, blueSpawn.Y);
+            };
+            cb.informPlayerJoin = delegate (int id, CTFTeam team) {
+                var tplr = TShock.Players[revID[id]];
+                if (team == CTFTeam.Red)
+                    announceRedMessage("{0} joined the red team!", tplr.Name);
+                else if (team == CTFTeam.Blue)
+                    announceBlueMessage("{0} joined the blue team!", tplr.Name);
+                else
+                    announceMessage("{0} joined the game.", tplr.Name);
+            };
+            cb.informPlayerRejoin = delegate (int id, CTFTeam team) {
+                Debug.Assert(team != CTFTeam.None);
+                var tplr = TShock.Players[revID[id]];
+                if (team == CTFTeam.Red)
+                    announceRedMessage("{0} rejoined the red team.", tplr.Name);
+                else
+                    announceBlueMessage("{0} rejoined the blue team.", tplr.Name);
+            };
+            cb.informPlayerLeave = delegate (int id, CTFTeam team) {
+                Debug.Assert(team != CTFTeam.None);
+                var tplr = TShock.Players[revID[id]];
+                if (team == CTFTeam.Red)
+                    announceRedMessage("{0} left the red team.", tplr.Name);
+                else
+                    announceBlueMessage("{0} left the blue team.", tplr.Name);
+            };
+            cb.announceGetFlag = delegate (int id, CTFTeam team) {
+                Debug.Assert(team != CTFTeam.None);
+                var tplr = TShock.Players[revID[id]];
+                // TODO - add crown to head
+                if (team == CTFTeam.Red) {
+                    // TODO - remove blue flag
+                    announceRedMessage("{0} is taking blue team's flag!", tplr.Name);
+                } else {
+                    // TODO - remove red flag
+                    announceBlueMessage("{0} is taking red team's flag!", tplr.Name);
+                }
+            };
+            cb.announceCaptureFlag = delegate (int id, CTFTeam team, int redScore, int blueScore) {
+                Debug.Assert(team != CTFTeam.None);
+                var tplr = TShock.Players[revID[id]];
+                // TODO - remove crown from head
+                if (team == CTFTeam.Red) {
+                    // TODO - add blue flag
+                    announceRedMessage("{0} captured blue team's flag and scored a point!", tplr.Name);
+                } else {
+                    // TODO - add red flag
+                    announceRedMessage("{0} captured red team's flag and scored a point!", tplr.Name);
+                }
+                announceScore(redScore, blueScore);
+            };
+            cb.announceFlagDrop = delegate (int id, CTFTeam team) {
+                Debug.Assert(team != CTFTeam.None);
+                var tplr = TShock.Players[revID[id]];
+                // TODO - remove crown from head
+                if (team == CTFTeam.Red) {
+                    // TODO - add blue flag
+                    announceRedMessage("{0} dropped blue team's flag.", tplr.Name);
+                } else {
+                    // TODO - add red flag
+                    announceBlueMessage("{0} dropped red team's flag.", tplr.Name);
+                }
+            };
+            cb.announceGameStart = delegate () {
+                announceMessage("The game has started! You have 5 minutes to prepare your base!");
+                // TODO - timer
+            };
+            cb.announceCombatStart = delegate () {
+                announceMessage("Preparation phase has ended! Capture the other team's flag!");
+                announceMessage("First team to get 2 points more than the other team wins!");
+                // TODO- timer
+            };
+            cb.announceGameEnd = delegate (CTFTeam winner, int redScore, int blueScore) {
+                announceMessage("The game has ended with score of {0} - {1}.", redScore, blueScore);
+                if (winner == CTFTeam.Red)
+                    announceRedMessage("Congratulations to red team!");
+                else if (winner == CTFTeam.Blue)
+                    announceBlueMessage("Congratulations to blue team!");
+                else
+                    announceMessage("Game ended in a draw.");
+            };
+            cb.tellPlayerTeam = delegate (int id, CTFTeam team) {
+                Debug.Assert(team != CTFTeam.None);
+                var tplr = TShock.Players[revID[id]];
+                // TODO - tell opponent direction
+                if (team == CTFTeam.Red)
+                    sendRedMessage(tplr, "You are on the red team.");
+                else
+                    sendBlueMessage(tplr, "You are on the blue team.");
+            };
+            cb.tellPlayerSelectClass = delegate (int id) {
+                var tplr = TShock.Players[revID[id]];
+                tplr.SendInfoMessage("Select your class with {0}class.");
+            };
+            cb.tellPlayerCurrentClass = delegate (int id, int cls) {
+                var tplr = TShock.Players[revID[id]];
+                throw new NotImplementedException();
+                // TODO - implement classes
+            };
+
             ctf = new CTFController(cb);
 
             // commands
@@ -77,7 +206,10 @@ namespace CGGCTF
         void onLogin(PlayerPostLoginEventArgs args)
         {
             var tplr = args.Player;
+            var ix = tplr.Index;
             var id = tplr.User.ID;
+
+            revID[id] = ix;
 
             originalChar[tplr.Index] = new PlayerData(tplr);
             originalChar[tplr.Index].CopyCharacter(tplr);
@@ -135,6 +267,38 @@ namespace CGGCTF
             var index = e.PlayerId;
             Main.player[index].hostile = playerPvP[index];
             NetMessage.SendData((int)PacketTypes.TogglePvp, -1, -1, "", index);
+        }
+        #endregion
+
+        #region Messages
+        void sendRedMessage(TSPlayer tplr, string msg, params object[] args)
+        {
+            tplr.SendMessage(msg.SFormat(args), 255, 102, 102);
+        }
+
+        void announceRedMessage(string msg, params object[] args)
+        {
+            sendRedMessage(TSPlayer.All, msg, args);
+        }
+
+        void sendBlueMessage(TSPlayer tplr, string msg, params object[] args)
+        {
+            tplr.SendMessage(msg.SFormat(args), 102, 178, 255);
+        }
+
+        void announceBlueMessage(string msg, params object[] args)
+        {
+            sendBlueMessage(TSPlayer.All, msg, args);
+        }
+        
+        void announceMessage(string msg, params object[] args)
+        {
+            TSPlayer.All.SendInfoMessage(msg, args);
+        }
+
+        void announceScore(int red, int blue)
+        {
+            announceMessage("Current Score | Red {0} - {1} Blue", red, blue);
         }
         #endregion
     }
